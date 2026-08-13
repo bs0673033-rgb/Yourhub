4f0de3c08968f4e31de1b4a817b3cf46009e39e3
bs0673033-rgb <bs0673033@gmail.com>
2026-08-13 11:52:51 +0530

    Create GuideModule.lua

---
+++ Utils/Blox fruit/GameModules/GuideModule.lua
@@ -0,0 +1,59 @@
+local GuideModule = {
+	Data = {
+		NPCs = {
+			-- First Sea
+			["BanditQuest1"] = "Bandit Quest Giver",
+			["JungleQuest"] = "Adventurer",
+			["BuggyQuest1"] = "Pirate Adventurer",
+			["DesertQuest"] = "Desert Adventurer",
+			["SnowQuest"] = "Villager",
+			["MarineQuest"] = "Marine Leader",
+			["MarineQuest2"] = "Marine",
+			["SkyQuest"] = "Sky Adventurer",
+			["PrisonerQuest"] = "Jail Keeper",
+			["ImpelQuest"] = "Head Jailer",
+			["ColosseumQuest"] = "Colosseum Quest Giver",
+			["MagmaQuest"] = "The Mayor",
+			["FishmanQuest"] = "King Neptune",
+			["SkyExp1Quest"] = "Mole",
+			["SkyExp2Quest"] = "Sky Quest Giver 2",
+			["FountainQuest"] = "Freezeburg Quest Giver",
+
+			-- Second Sea
+			["Area1Quest"] = "Area 1 Quest Giver",
+			["Area2Quest"] = "Area 2 Quest Giver",
+			["MarineQuest3"] = "Marine Quest Giver",
+			["ZombieQuest"] = "Graveyard Quest Giver",
+			["SnowMountainQuest"] = "Snow Quest Giver",
+			["IceSideQuest"] = "Ice Quest Giver",
+			["FireSideQuest"] = "Fire Quest Giver",
+			["ShipQuest1"] = "Rear Crew Quest Giver",
+			["ShipQuest2"] = "Front Crew Quest Giver",
+			["FrostQuest"] = "Frost Quest Giver",
+			["ForgottenQuest"] = "Forgotten Quest Giver",
+
+			-- Third Sea
+			["PiratePortQuest"] = "Pirate Port Quest Giver",
+			["DeepForestIsland"] = "Deep Forest Quest Giver",
+			["DeepForestIsland2"] = "Deep Forest Area 2 Quest Giver",
+			["DeepForestIsland3"] = "Turtle Adventure Quest Giver",
+			["MarineTreeIsland"] = "Marine Tree Quest Giver",
+			["CastleQuest1"] = "Haunted Castle Quest Giver 1",
+			["CastleQuest2"] = "Haunted Castle Quest Giver 2",
+			["NutsIslandQuest"] = "Peanut Quest Giver",
+			["IceCreamIslandQuest"] = "Ice Cream Quest Giver",
+			["CakeQuest1"] = "Cake Quest Giver 1",
+			["CakeQuest2"] = "Cake Quest Giver 2",
+			["ChocQuest1"] = "Chocolate Quest Giver 1",
+			["ChocQuest2"] = "Chocolate Quest Giver 2",
+			["CandyQuest1"] = "Candy Cane Quest Giver",
+			["TikiQuest1"] = "Tiki Quest Giver 1",
+			["TikiQuest2"] = "Tiki Quest Giver 2",
+			["TikiQuest3"] = "Tiki Quest Giver 3",
+			["VenomCrewQuest"] = "Hydra Town Quest Giver",
+			["DragonCrewQuest"] = "Dragon Crew Quest Giver",
+			["HornedMan"] = "Horned Man",
+			["ArenaTrainer"] = "Arena Trainer",
+		}
+	}
+}
